3139c0d444f35114424b2d289f341e46285e3282
saeed karimi <s_karimi20@outlook.com>
2020-10-30 20:35:33 +0330

    make it better

--- routers/register.js
+++ routers/register.js
@@ -84,4 +84,36 @@ router.post('/user',async (req,res,next) =>{
 
 
 
+// router.get('/me', auth, async (req, res) => {
+//     const user = await User.findById(req.user._id).select('-password');
+//     res.send(user);
+//   });
+
+// router.post('/', async (req,res,next) =>{
+
+//     if(!req.body.name || !req.body.number || !req.body.password){
+//         res.status(400).send('Please Enter name, username , password');
+//     }
+//     let user = await User.findOne({ username : req.body.username });
+//     if (user){
+//         return res.status(400).send('Already Registered');
+//     } else{
+//         user = new User(_.pick(req.body, ['name', 'username', 'password']));
+//         const salt =  await bcrypt.genSalt(10);
+//         user.password =  await bcrypt.hash(user.password, salt);
+//         await user.save();
+      
+//         const token = user.generateAuthToken();
+//         // res.header('x-auth-token', token).send(_.pick(user, ['_id']));
+//         res.status(200).json({token: token});
+
+//     }
+
+// });
+
+
+
+
+
+
 module.exports = router; 
