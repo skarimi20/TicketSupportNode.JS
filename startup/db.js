38c8edb3eb2d9c6074a93e0c2df48f40d1781cd3
saeed karimi <s_karimi20@outlook.com>
2020-10-30 20:55:14 +0330

    make it better

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
+  const dbLink = `mongodb+srv://saeed_karimi20:${config.get('db_password')}@ticket.23qub.mongodb.net/Ticket?retryWrites=true&w=majority`
+  const db = config.get(dbLink);
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
