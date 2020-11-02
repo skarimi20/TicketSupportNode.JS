eaa077773dddbabf1c75443106d358fae5c2403e
saeed karimi <s_karimi20@outlook.com>
2020-11-02 14:54:48 +0330

    master

--- startup/db.js
+++
@@ -1,9 +0,0 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
-
-module.exports = function() {
-  const dbLink = `mongodb+srv://saeed_karimi20:${config.get('db_password')}@ticket.23qub.mongodb.net/Ticket?retryWrites=true&w=majority`
-  mongoose.connect(dbLink)
-    .then(() => winston.info(`Connected to ...`));
-}
